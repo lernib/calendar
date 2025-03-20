0ac0a84aa437d53ff286573d2cd737c4aed43764
Carlos Kieliszewski <ckieliszewski@lernib.com>
2025-03-19 21:08:16 -0400

    0.3.0

--- package/src/calendar.ts
+++ package/src/calendar.ts
@@ -11,6 +11,11 @@ export class Calendar {
 		return this._events;
 	}
 
+	/**
+	 * Insert an event into the calendar. This can be modified after the fact without re-insertion.
+	 * @param params The arguments for creating the initial event.
+	 * @returns The event that can be modified in-place.
+	 */
 	public insert_at(...params: EventConstructorParams): Event {
 		const event = new Event(...params);
 
@@ -30,6 +35,11 @@ export class Calendar {
 		return event;
 	}
 
+	/**
+	 * Gets all events in the calendar before a specific date.
+	 * @param date The date to get events before.
+	 * @returns All events before that date.
+	 */
 	public all_before(date: string): Event[] {
 		const events = this._events
 			.flatMap((e) => e.all_before(date))
@@ -38,6 +48,11 @@ export class Calendar {
 		return events;
 	}
 
+	/**
+	 * Moves the calendar to after the specified date/datetime.
+	 * @param params The arguments to Event.next_after
+	 * @returns The updated calendar. This creates a clone.
+	 */
 	public after(...params: NextEventAfterParams): Calendar {
 		const events = this._events.map((e) => e.next_after(...params));
 		return new Calendar(events);
